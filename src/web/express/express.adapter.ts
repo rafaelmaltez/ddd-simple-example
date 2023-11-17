5fe1053764a09190858b70d559c3a858c3e032f1
Rafael Maltez <rafaelmaltez89@gmail.com>
2023-11-17 12:24:36 -0300

    fix express adapter success return format

--- src/web/express/express.adapter.ts
+++ src/web/express/express.adapter.ts
@@ -12,7 +12,7 @@ export class ExpressAdapter implements HttpServer {
         this.app[method](url, async (req: Request, res: Response) => {
             try {
                 const token = await callback(null, req.body)
-                return res.status(200).json({ token })
+                return res.status(200).json(token)
             } catch (error: any) {
                 res.status(422).json({ message: error.message })
             }
