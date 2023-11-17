c60b216d9e205beff7fe4c459fc71f839fe32594
Rafael Maltez <rafaelmaltez89@gmail.com>
2023-11-17 12:25:14 -0300

    remove unnecessary console.log calls from hono adapter

--- src/web/hono/hono.adapter.ts
+++ src/web/hono/hono.adapter.ts
@@ -10,16 +10,13 @@ export class HonoAdapter implements HttpServer {
   }
 
   on(method: string, url: string, callback: Function): void {
-    console.log("method", method)
-    console.log("url", url);
     this.app[method](url, async (c: any) => {
       try {
         const body = await c.req.json();
         const result = await callback(null, body);
         c.status(200)
         return c.json(result);
-      } catch (error: any) {
-        console.log("ciu no catch", error)
+      } catch (error: any) {    
         c.status(422)
         return c.json({ message: error.message });
       }
